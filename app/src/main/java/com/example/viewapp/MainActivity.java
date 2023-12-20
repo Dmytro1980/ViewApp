8e5a769eb40c6c26aa25135cfece3b39738577e5
Dmytro1980 <dm.st.mazda@gmail.com>
2023-12-20 16:15:42 +0200

    FrameLayout

--- app/src/main/java/com/example/viewapp/MainActivity.java
+++ app/src/main/java/com/example/viewapp/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.viewapp;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,23 +13,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     // ! ! ! ! !
-
-    // TableLayout
-
+    // FrameLayout
     // ! ! ! ! !
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
         // для использования разметки из activity_main.xml
         // ! ! ! ! ! ! ! ! ! !
         setContentView(R.layout.activity_main);
         // ! ! ! ! ! ! ! ! ! !
+
+
+
 
     }
 }
