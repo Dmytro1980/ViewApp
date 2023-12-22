7fe8d1f812ea832306d5df998c33d75f289819de
Dmytro1980 <dm.st.mazda@gmail.com>
2023-12-22 17:25:15 +0200

    GridLayout

--- app/src/main/java/com/example/viewapp/MainActivity.java
+++ app/src/main/java/com/example/viewapp/MainActivity.java
@@ -2,31 +2,42 @@ package com.example.viewapp;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.GridLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     // ! ! ! ! !
-    // FrameLayout
+    // GridLayout
     // ! ! ! ! !
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // для использования разметки из activity_main.xml
-        // ! ! ! ! ! ! ! ! ! !
-        setContentView(R.layout.activity_main);
-        // ! ! ! ! ! ! ! ! ! !
+//        // для использования разметки из activity_main.xml
+//        // ! ! ! ! ! ! ! ! ! !
+//        setContentView(R.layout.activity_main);
+//        // ! ! ! ! ! ! ! ! ! !
 
 
 
+        // програмное создание GridLayout
+        GridLayout gridLayout = new GridLayout(this);
+
+        // установка количество строк
+        gridLayout.setRowCount(3);
+        // установка количество столбцов
+        gridLayout.setColumnCount(3);
+
+        for (int i = 1; i <= 9; i++) {
+            Button button = new Button(this);
+            button.setText(String.valueOf(i));
+            gridLayout.addView(button);
+        }
+
+        setContentView(gridLayout);
 
     }
 }
